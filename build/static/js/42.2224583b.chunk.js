d39114cef5b9e8a854a9ad19051634792f6fcbd6
“FE-dev1” <pankajagrawaltw@gmail.com>
2022-01-13 21:02:20 +0530

    Production build update 13 Jan

---
+++ build/static/js/42.2224583b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpshekung-frontend"]=this["webpackJsonpshekung-frontend"]||[]).push([[42],{688:function(e,n,t){},868:function(e,n,t){"use strict";t.r(n);t(1);var s=t(164),c=(t(270),t(688),t(0));n.default=function(){return Object(c.jsx)(c.Fragment,{children:Object(c.jsxs)("div",{className:"noselect",children:[Object(c.jsx)(s.i,{title:"She\u0915\u0941\u0902\u091c - Login"}),Object(c.jsx)(s.g,{loginPage:!0}),Object(c.jsx)(s.b,{loginPage:!0}),Object(c.jsx)(s.e,{loginPage:!0})]})})}}}]);
+//# sourceMappingURL=42.2224583b.chunk.js.map
