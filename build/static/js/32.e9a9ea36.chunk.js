6b895372e7e44c82eb35db0087698fdf8a9f7df3
“FE-dev1” <pankajagrawaltw@gmail.com>
2022-01-18 20:10:27 +0530

    Staging build update 18 Jan

--- build/static/js/32.e9a9ea36.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpshekunj-frontend"]=this["webpackJsonpshekunj-frontend"]||[]).push([[32],{598:function(e,n,t){},687:function(e,n,t){},867:function(e,n,t){"use strict";t.r(n);t(1),t(598);var s=t(164),c=(t(687),t(0));n.default=function(){return Object(c.jsx)(c.Fragment,{children:Object(c.jsxs)("div",{className:"noselect",children:[Object(c.jsx)(s.i,{title:"She\u0915\u0941\u0902\u091c - Signup"}),Object(c.jsx)(s.g,{loginPage:!0}),Object(c.jsx)(s.b,{}),Object(c.jsx)(s.e,{loginPage:!0})]})})}}}]);
-//# sourceMappingURL=32.e9a9ea36.chunk.js.map
--- build/static/js/44.1f36cbf0.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpshekunj-frontend"]=this["webpackJsonpshekunj-frontend"]||[]).push([[44],{879:function(n,t,e){"use strict";e.r(t);e(1);var s=e(30),c=e(0);t.default=function(){return Object(c.jsx)("div",{children:Object(c.jsx)(s.a,{to:"/"})})}}}]);
-//# sourceMappingURL=44.1f36cbf0.chunk.js.map
