83e9fa9c25f67493667577591ced6d5da9f5aabd
nitinthoughtwin <nitin.thoughtwin@gmail.com>
2022-01-28 12:53:32 +0530

    Staging build

--- build/static/js/46.a75757d3.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpshekunj-frontend"]=this["webpackJsonpshekunj-frontend"]||[]).push([[46],{881:function(n,t,e){"use strict";e.r(t);e(1);var s=e(30),c=e(0);t.default=function(){return Object(c.jsx)("div",{children:Object(c.jsx)(s.a,{to:"/"})})}}}]);
-//# sourceMappingURL=46.a75757d3.chunk.js.map
